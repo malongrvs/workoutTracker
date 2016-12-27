a48179d190a12c20bf0bde1e77c44550fce3112a
Reiss Greaves <Reiss Greaves>
2016-12-27 16:45:04 +0000

    Leaving home

--- app/src/main/java/com/grvs/malon/workouttracker/HomeActivity.java
+++ app/src/main/java/com/grvs/malon/workouttracker/HomeActivity.java
@@ -2,9 +2,10 @@ package com.grvs.malon.workouttracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
-
+    public static TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
